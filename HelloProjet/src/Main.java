c440ab4e49c98d94ddbd61c6a24a6325c28033b5
Fan <609485623@qq.com>
2019-07-10 18:59:29 +0800

    修改了main.java

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,4 +6,10 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
     }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("ssssss");
+    }
 }
